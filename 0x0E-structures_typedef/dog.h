95a7987bd29385acf8f4a13f7c834ca941cde2ab
abdo <abdogendy2001@gmail.com>
2023-11-06 19:25:26 +0200

    update2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
 #ifndef DOG.H
 #define DOG.H
 
+/**
+ *struct dog - a dog struct
+ @name : dog's name
+ @age : dog's age
+ @owner : owner's name
+ *
+ * description : just a lone dot struct in a big kitty world
+ *
+ */
+
+
 struct dog 
 {
   char *name;
